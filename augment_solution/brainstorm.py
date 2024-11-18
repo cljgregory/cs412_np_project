a2e20dc3c75c531ed18d34b3a98b8614f948f7e6
Austin Perdue <perdueca@dukes.jmu.edu>
2024-11-18 11:08:22 -0500

    brain storm changes

---
+++ augment_solution/brainstorm.py
@@ -0,0 +1,13 @@
+# DR MOLLOY SUGGESTIONS FOR AUGMENTING APPROX SOLUTION
+# FOR MIN GRAPH COLORING NP PROJECT
+
+# lower bound, papers about lower bounding, 
+# lower bound (doesn't hve to be the best lower bound), 
+# compare approximation, show how they differ, 
+# discuss how they differ, max clique = min color, 
+# talk about how they're related, max clique and min color are the same problem, 
+# generate a bunch of test cases how long to break approximation algorithm, 
+# exact solution should run in a reasonably amount of time, 
+# run two programs (exact/approx) are they the same? are they different? 
+# generate test cases/generate graphs, run the other two programs, 
+# google around for np complete min graph coloring to solve 3sat (reduction)
