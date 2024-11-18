148edbd45644b533848ae179bd70ad82c8be1518
Colin Gregory <gregorcl@dukes.jmu.edu>
2024-11-18 10:56:46 -0500

    Nov 18 comments for exact solution

---
+++ exact_solution/min_coloring_exact.py
@@ -0,0 +1,11 @@
+# IDEAS FOR EXACT SOLUTION
+# Find every possible coloring of the graph regardless of if it is correct or not
+# Bounds for coloring will be 1 to n where n is the number of vertices
+# Sift through all possible colorings and check if they are valid using 2 loops?
+# If valid, check if it is the minimum coloring
+# If it is, store it and continue checking for other colorings
+# If it is not, continue checking for other colorings
+# Return the minimum coloring found
+
+
+
